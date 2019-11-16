555888a8dd1bc48cfc9222ba7294a8da35d039bd
Jamie Luong <jamieluong03@gmail.com>
2019-11-15 23:24:47 -0800

    changed DataArea using hooks

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,105 +1,106 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import DataTable from "./DataTable";
 import Nav from "./Nav";
 import API from "../utils/API";
 import "../styles/DataArea.css";
 
-export default class DataArea extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [{}],
-      order: "descend",
-      filteredUsers: [{}],
-      headings: [
-        { name: "Image", width: "10%" },
-        { name: "Name", width: "10%" },
-        { name: "Phone", width: "20%" },
-        { name: "Email", width: "20%" },
-        { name: "DOB", width: "10%" }
-      ],
+const DataArea = () => {
 
-      handleSort: heading => {
+  const [users, setUsers] = useState([{}]);
+  const [order, setOrder] = useState("descend");
+  const [filteredUsers, setFilteredUsers] = useState([{}]);
+  const [headings, setHeadings] = useState([
+    { name: "Image", width: "10%" },
+    { name: "Name", width: "10%" },
+    { name: "Phone", width: "20%" },
+    { name: "Email", width: "20%" },
+    { name: "DOB", width: "10%" }
+  ])
 
-        if (this.state.order === "descend") {
-          this.setState({
-            order: "ascend"
-          })
-        } else {
-          this.setState({
-            order: "descend"
-          })
-        }
-
-        const compareFnc = (a, b) => {
-          if (this.state.order === "ascend") {
-            // account for missing values
-            if (a[heading] === undefined) {
-              return 1;
-            } else if (b[heading] === undefined) {
-              return -1;
-            }
-            // numerically
-            else if (heading === "name") {
-              return a[heading].first.localeCompare(b[heading].first);
-            } else {
-              return a[heading] - b[heading];
-            }
-          } else {
-            // account for missing values
-            if (a[heading] === undefined) {
-              return 1;
-            } else if (b[heading] === undefined) {
-              return -1;
-            }
-            // numerically
-            else if (heading === "name") {
-              return b[heading].first.localeCompare(a[heading].first);
-            } else {
-              return b[heading] - a[heading];
-            }
-          }
-
-        }
-        const sortedUsers = this.state.filteredUsers.sort(compareFnc);
-        this.setState({ filteredUsers: sortedUsers });
-      },
-      handleSearchChange: event => {
-        console.log(event.target.value);
-        const filter = event.target.value;
-        const filteredList = this.state.users.filter(item => {
-          // merge data together, then see if user input is anywhere inside
-          let values = Object.values(item)
-            .join("")
-            .toLowerCase();
-          return values.indexOf(filter.toLowerCase()) !== -1;
-        });
-        this.setState({ filteredUsers: filteredList });
-      }
-    };
+const handleSort = heading => {
+  console.log(heading);
+  if (order === "descend") {
+    setOrder({
+      order: "ascend"
+    })
+  } else {
+    setOrder({
+      order: "descend"
+    })
   }
 
-  componentDidMount() {
+  const compareFnc = (a, b) => {
+    if (order === "ascend") {
+      // account for missing values
+      if (a[heading] === undefined) {
+        return 1;
+      } else if (b[heading] === undefined) {
+        return -1;
+      }
+      // numerically
+      else if (heading === "name") {
+        return a[heading].first.localeCompare(b[heading].first);
+      } else {
+        return a[heading] - b[heading];
+      }
+    } else {
+      // account for missing values
+      if (a[heading] === undefined) {
+        return 1;
+      } else if (b[heading] === undefined) {
+        return -1;
+      }
+      // numerically
+      else if (heading === "name") {
+        return b[heading].first.localeCompare(a[heading].first);
+      } else {
+        return b[heading] - a[heading];
+      }
+    }
+
+  }
+    const sortedUsers = filteredUsers.sort(compareFnc);
+    setFilteredUsers({ filteredUsers: sortedUsers });
+  };
+
+const handleSearchChange = event => {
+  console.log(event.target.value);
+  const filter = event.target.value;
+  const filteredList = users.filter(item => {
+    // merge data together, then see if user input is anywhere inside
+    let values = Object.values(item)
+      .join("")
+      .toLowerCase();
+    return values.indexOf(filter.toLowerCase()) !== -1;
+  });
+  setFilteredUsers({ filteredUsers: filteredList });
+  };
+
+
+  useEffect(() => {
     API.getUsers().then(results => {
-      this.setState({
+      // console.log(results);
+      setUsers({
         users: results.data.results,
+      });
+      setFilteredUsers({
         filteredUsers: results.data.results
       });
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        <Nav handleSearchChange={this.state.handleSearchChange} />
-        <div className="data-area">
-          <DataTable
-            headings={this.state.headings}
-            users={this.state.filteredUsers}
-            handleSort={this.state.handleSort}
-          />
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+    <Nav handleSearchChange={handleSearchChange} />
+    <div className="data-area">
+      <DataTable
+        headings={headings}
+        users={filteredUsers}
+        handleSort={handleSort}
+      />
+    </div>
+    </>
+  );
 }
+
+export default DataArea;
